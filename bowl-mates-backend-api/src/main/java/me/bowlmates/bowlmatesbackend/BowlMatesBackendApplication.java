dec380607a56ecf66f04735370def9cf8ea492cf
Daniel Johnson <danieldrdj01@gmail.com>
2023-10-22 02:45:47 -0700

    Added packages containing backend & frontend
    The packages added are called:
    - bowl-mates-backend-api
      *Navigate to the src/main/java/me.bowlmates.bowlmatesbackend/BowlMatesBackendApplication file and start the backend service from there using the green start button in intellij
    
    - bowl-mates-frontend
       *Navigate to the bowl-mates-frontend directory and type "npm start" to start the frontend service
    
    The frontend is a react typescript server (can easily be removed and changed to a regular react javascript project later if it is decided that typescript is not going to be used for this application)
    
    The backend is a springboot java server

---
+++ bowl-mates-backend-api/src/main/java/me/bowlmates/bowlmatesbackend/BowlMatesBackendApplication.java
@@ -0,0 +1,13 @@
+package me.bowlmates.bowlmatesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlMatesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlMatesBackendApplication.class, args);
+    }
+
+}
---
+++ bowl-mates-backend-api/src/test/java/me/bowlmates/bowlmatesbackend/BowlMatesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bowlmates.bowlmatesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlMatesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
