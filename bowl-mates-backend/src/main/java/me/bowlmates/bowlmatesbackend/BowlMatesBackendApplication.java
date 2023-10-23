51b4cfdecd994bd0cb66bde0157c9c422eb0d83f
Daniel Johnson <danieldrdj01@gmail.com>
2023-10-22 17:13:41 -0700

    Backend module recreated
    Some naming issues were preventing the server from running so I re-made it instead.

--- bowl-mates-backend/src/main/java/me/bowlmates/bowlmatesbackend/BowlMatesBackendApplication.java
+++ bowl-mates-backend/src/main/java/me/bowlmates/bowlmatesbackend/BowlMatesBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BowlMatesBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BowlMatesBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BowlMatesBackendApplication.class, args);
+	}
 
 }
