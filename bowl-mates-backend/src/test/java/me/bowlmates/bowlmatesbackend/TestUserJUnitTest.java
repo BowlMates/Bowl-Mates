594a30a6848867b90b3be40f89f92e6f0ea0b087
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-10-31 12:28:49 -0700

    TestUser JUnit tests

--- bowl-mates-backend/src/test/java/me/bowlmates/bowlmatesbackend/TestUserJUnitTest.java
+++ bowl-mates-backend/src/test/java/me/bowlmates/bowlmatesbackend/TestUserJUnitTest.java
@@ -1,6 +1,48 @@
 package me.bowlmates.bowlmatesbackend;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestUserJUnitTest {
+
+    TestUser u1;
+    
+    @BeforeAll
+    private void setUp() {
+        u1 = new TestUser();
+    }
+
+    @Test
+    public void TestUserNameFunctions() {
+        u1.setName("u1");
+        assertEquals(u1.getName(), "u1");
+    }
+
+    @Test
+    public void TestUserIDFunctions() {
+        u1.setId(1);
+        assertEquals(u1.getId(), 1);
+    }
+
+    @Test
+    public void TestUserEmailFunctions() {
+        u1.setEmail("u1@mail.com");
+        assertEquals(u1.getEmail(), "u1@mail.com");
+    }
+
+    @Test
+    public void TestUsernameFunctions() {
+        u1.setUsername("u1");
+        assertEquals(u1.getUsername(), "u1");
+    }
+
+    @Test
+    public void TestPasswordFunctions() {
+        u1.setPassword("password");
+        assertEquals(u1.getPassword(), "password");
+    }
 }
