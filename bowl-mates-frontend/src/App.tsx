2e37b6432bf3d9a68c3045cad184e59c79949dea
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-11-03 16:03:32 -0700

    Update App.tsx

--- bowl-mates-frontend/src/App.tsx
+++ bowl-mates-frontend/src/App.tsx
@@ -5,7 +5,16 @@ import SignIn from "./SignIn";
 
 function App () {
   //return SignIn;
-    let str = () => getString();
+    // fetch('https://catfact.ninja/fact').then(response => {
+    //     return response.json();
+    // }).then(data => {
+    //     console.log(data);
+    //     str = data;
+    // }).catch(error =>
+    // {
+    //     str = "bad";
+    // });
+    let str = getString().then().toString();
   return (
     <div className='App'>
       <header className='App-header'>
@@ -22,26 +31,38 @@ function App () {
         {/*  Learn React*/}
         {/*</a>*/}
           {str}
+          "Header goes here"
       </header>
     </div>
   )
 }
 
 async function getString() {
-  try {
-      let response : any = await fetch("localhost:8080/all").then((res)=>{
-          if (!response.ok) {
-              alert("Uh oh! Status is: " + response.status);
-              return "";
-          }
-          return response.json;
-      }).then((text)=>{
-          return text;
-      });
-  } catch (e) {
-      alert("There was an error contacting the server.");
-      console.log(e);
-  }
+    let str;
+    await fetch('http://localhost:8080/test').then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+        str = data;
+    }).catch(error =>
+    {
+        str = "bad";
+    });
+    return str;
+  // try {
+  //     let response : any = await fetch("https://catfact.ninja/fact").then((res)=>{
+  //         if (!response.ok) {
+  //             alert("Uh oh! Status is: " + response.status);
+  //             return "";
+  //         }
+  //         return response.json;
+  //     }).then((text)=>{
+  //         return text;
+  //     });
+  // } catch (e) {
+  //     alert("There was an error contacting the server.");
+  //     console.log(e);
+  // }
 }
 
 export default App
