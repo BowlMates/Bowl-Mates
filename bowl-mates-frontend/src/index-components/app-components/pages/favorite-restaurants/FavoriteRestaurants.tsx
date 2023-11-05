778c3344be75b9780f98b1a5004db3c4ca5a5c6d
Daniel Johnson <danieldrdj01@gmail.com>
2023-11-05 02:43:00 -0800

    Adjusted template pages

--- bowl-mates-frontend/src/index-components/app-components/pages/favorite-restaurants/FavoriteRestaurants.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/favorite-restaurants/FavoriteRestaurants.tsx
@@ -1,8 +1,22 @@
 // MUI Imports
-
+import {styled, useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
+//Pre-Styling
+//----------------------------------------------------------------------------
+// You can pre-style components using the styled method/function
+// Place the component type you want styled as an argument (in this case - Box)
+// and then style the inside as if it were in-line styling or styling in a css
+// file
+const ExampleStyledComponent = styled(Box)(({ theme }) => ({
+    flexGrow: 1,
+    marginTop: "64px",
+    p: 3, //padding
+    backgroundColor: theme.palette.primary.main,
+    height: "calc(100% - 64px)",
+    width: "auto"
+}));
 
 function FavoriteRestaurants () {
 
@@ -15,21 +29,6 @@ function FavoriteRestaurants () {
     //      this family of components since they will be most cohesive together
     //      while also allowing us to change theming easier and possibly implement
     //      dark theme functionality
-
-    //Pre-Styling
-    //----------------------------------------------------------------------------
-    // You can pre-style components using the styled method/function
-    // Place the component type you want styled as an argument (in this case - Box)
-    // and then style the inside as if it were in-line styling or styling in a css
-    // file
-    const ExampleStyledComponent = styled(Box)(({ theme }) => ({
-        flexGrow: 1,
-        marginTop: "64px",
-        p: 3, //padding
-        backgroundColor: theme.palette.primary.main,
-        height: "calc(100% - 64px)",
-        width: "auto"
-    }));
 
     return (
         <Typography variant={"h1"}>
