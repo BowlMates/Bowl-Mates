d1788515161133f56e98254e62b9b340061fc668
jbalinas <108829766+jbalinas@users.noreply.github.com>
2023-11-05 17:50:03 -0800

    trying makeStyles

--- bowl-mates-frontend/src/index-components/app-components/pages/availability/Availability.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/availability/Availability.tsx
@@ -2,7 +2,7 @@
 import {styled, useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import {makeStyles} from "@mui/material";
+import {makeStyles} from "@mui/material/styles";
 
 //Pre-Styling
 //----------------------------------------------------------------------------
