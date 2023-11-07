a76024c4dc36c10632821e7568a140421d131a6d
Geoffrey Aldrich <87826956+geoffreyaldrich@users.noreply.github.com>
2023-11-07 09:26:04 -0800

    Update Settings.tsx

--- bowl-mates-frontend/src/index-components/app-components/pages/settings/Settings.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/settings/Settings.tsx
@@ -1,7 +1,11 @@
 // MUI Imports
 import {styled, useTheme} from "@mui/material/styles";
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
 
 //Pre-Styling
 //----------------------------------------------------------------------------
@@ -30,10 +34,38 @@ function Settings () {
     //      while also allowing us to change theming easier and possibly implement
     //      dark theme functionality
 
+
+    // need to add photo upload functionality
     return (
-        <Typography variant={"h1"}>
-            This is the settings page!
-        </Typography>
+        <Container maxWidth="sm">
+            <Typography variant="h1">
+                give us the details
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <TextField id="filled-basic" label="display name" variant="filled" fullWidth={true} inputProps={{ maxLength: 30 }}/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField id="filled-basic" label="pronouns" variant="filled" fullWidth={true} inputProps={{ maxLength: 30 }}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        id="filled-multiline-static"
+                        label="bio"
+                        multiline
+                        rows={4}
+                        defaultValue="don't be shy, give the people something to relate to!"
+                        variant="filled"
+                        fullWidth={true}
+                        inputProps={{ maxLength: 300 }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained">submit</Button>
+                </Grid>
+            </Grid>
+        </Container>
+
     )
 }
 
