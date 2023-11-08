121a3c1ce2f82268a34e33c9f04a51a785aa34d2
jbalinas <108829766+jbalinas@users.noreply.github.com>
2023-11-08 09:27:21 -0800

    Signup done

--- bowl-mates-frontend/src/index-components/app-components/pages/signup/Signup.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/signup/Signup.tsx
@@ -184,7 +184,6 @@ function Signup() {
                         }}
                     />
 
-
                 </Rectangle>
                 <Rectangle bgcolor="#54804D">
                     <Typography
