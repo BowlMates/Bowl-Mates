8eaa0c43bbd330fc44c2423ee85642c6c297fb1e
Daniel Johnson <danieldrdj01@gmail.com>
2023-11-16 13:34:43 -0800

    User Chat API test example changed to include URL photos of users

--- bowl-mates-frontend/src/index-components/app-components/pages/successful-matches/SuccessfulMatches.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/successful-matches/SuccessfulMatches.tsx
@@ -9,31 +9,34 @@ import Typography from "@mui/material/Typography";
 // Place the component type you want styled as an argument (in this case - Box)
 // and then style the inside as if it were in-line styling or styling in a css
 // file
-const ExampleStyledComponent = styled(Box)(({ theme }) => ({
-    flexGrow: 1,
-    marginTop: "64px",
-    p: 3, //padding
-    backgroundColor: theme.palette.primary.main,
-    height: "calc(100% - 64px)",
-    width: "auto"
+const SuccessfulMatchesContainer = styled(Box)(({ theme }) => ({
+    display : "flex",
+    height : "calc(100% + 20px)",
+    width : "calc(100% + 40px)",
+    margin : "-20px",
 }));
+
+const ChatSelectionBox = styled(Box)(({ theme }) => ({
+    height : "calc(100% + 20px)",
+    width : "280px",
+    backgroundColor : "black",
+}));
+
+const ChatBox = styled(Box)(({ theme }) => ({
+    height : "calc(100% + 20px)",
+    width : "calc(100vw - 280px)",
+    backgroundColor : "purple",
+}));
+
+
 
 function SuccessfulMatches () {
 
-    //Notes about some MUI component types you will probably use the most
-    //----------------------------------------------------------------------------
-    //Note: Box is a better div (pls don't use divs)
-    //Note: Typography is a better version of html text tags (h1, p, etc...).
-    //      you can set the type of typography using variant={"h1"} within the tag
-    //Note: There is usually an MUI replacement for everything so try to stick with
-    //      this family of components since they will be most cohesive together
-    //      while also allowing us to change theming easier and possibly implement
-    //      dark theme functionality
-
     return (
-        <Typography variant={"h1"}>
-            This is the successful matches page!
-        </Typography>
+        <SuccessfulMatchesContainer>
+            <ChatSelectionBox></ChatSelectionBox>
+            <ChatBox></ChatBox>
+        </SuccessfulMatchesContainer>
     )
 }
 
