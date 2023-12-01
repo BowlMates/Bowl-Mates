587a950a19bafa214768604f7a077d2840d5e56c
Stephen Cushing <73804392+scushing@users.noreply.github.com>
2023-12-01 14:30:23 -0800

    Create MatchDTO.java

---
+++ bowl-mates-backend/src/main/java/me/bowlmates/bowlmatesbackend/Models/MatchDTO.java
@@ -0,0 +1,12 @@
+package me.bowlmates.bowlmatesbackend.Models;
+
+public class MatchDTO {
+
+    private Integer matchID;
+    private String matchFirstName;
+    private String matchLastName;
+    private String pronouns;
+    private String matchPhotoURL;
+    
+    // TODO : Implement Methods
+}
