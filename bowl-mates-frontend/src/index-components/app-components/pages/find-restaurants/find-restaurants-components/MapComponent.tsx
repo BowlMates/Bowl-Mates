f3e9c77692877e76f23f59cad6fe672c4579256d
Cade Dillon <cade.dillon@outlook.com>
2023-12-03 15:50:54 -0800

    Added pop up info window for map marker on cllick

--- bowl-mates-frontend/src/index-components/app-components/pages/find-restaurants/find-restaurants-components/MapComponent.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/find-restaurants/find-restaurants-components/MapComponent.tsx
@@ -1,7 +1,9 @@
 // MapComponent.js
 import React from 'react';
-import {GoogleMap, MarkerF, useJsApiLoader} from '@react-google-maps/api';
+import {GoogleMap, InfoWindow, MarkerF, useJsApiLoader} from '@react-google-maps/api';
 import {restaurant} from "../../../../../data-types/restaurants";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
 
 const mapContainerStyle = {
     width: '100%',
@@ -15,6 +17,13 @@ function MapComponent({restaurants, userLocation}: {restaurants: restaurant[], u
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyDXlQY2uFzDvS7HRowdgflkRqWtmKqYaGw"
     })
+
+    const [selectedMarker, setSelectedMarker] = React.useState<restaurant | null>(null);
+
+    const handleMarkerClick = (restaurant: restaurant) => {
+        setSelectedMarker(restaurant);
+    };
+
 
     const [map, setMap] = React.useState<google.maps.Map | null>(null)
 
@@ -38,13 +47,44 @@ function MapComponent({restaurants, userLocation}: {restaurants: restaurant[], u
                                         lng: parseFloat(restaurant.longitude.toString())
                                 }}
                                     title={restaurant.name}
+                                    onClick={() => handleMarkerClick(restaurant)}
                                 />
                             ))
                     ) : (
                         <div> </div>
                     )}
                 </div>
-
+                {selectedMarker && (
+                    <InfoWindow
+                        position={{
+                            lat: parseFloat(selectedMarker.latitude.toString()),
+                            lng: parseFloat(selectedMarker.longitude.toString())
+                        }}
+                        onCloseClick={() => setSelectedMarker(null)}
+                    >
+                        <div>
+                            <CardContent>
+                                <img
+                                    src={selectedMarker.reference}
+                                    alt={`${selectedMarker.name}`}
+                                    style={{ width: '100%', height: 'auto', maxHeight: '200px', objectFit: 'cover' }}
+                                />
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {selectedMarker.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Type: {selectedMarker.cuisine}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Address: {selectedMarker.address}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Rating: {selectedMarker.rating}
+                                </Typography>
+                            </CardContent>
+                        </div>
+                    </InfoWindow>
+                )}
             </GoogleMap>
     ) : <></>;
 }
