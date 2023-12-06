88c3bd165b6532e1dc263c8426f59b899a8ba46a
jbalinas <108829766+jbalinas@users.noreply.github.com>
2023-12-05 19:39:45 -0800

    Update Home.tsx
    Added accordion with instructions on how to use the site, moved buttons to the bottom

--- bowl-mates-frontend/src/index-components/app-components/pages/home/Home.tsx
+++ bowl-mates-frontend/src/index-components/app-components/pages/home/Home.tsx
@@ -5,6 +5,11 @@ import photo from '../../../../images/map-2.png';
 // MUI Imports
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 // Custom Imports
 import {useIsUserSessionValid} from "../../../../hooks/useIsUserSessionValid";
@@ -28,13 +33,58 @@ const Home = () => {
                 Welcome back, {authUser()!.firstName}!
             </Typography>
             {/*change to https://bowlmates.me/app/ later on*/}
+
+            {/* Div to control the size and position of the Accordion */}
+            <div style={{
+                maxWidth: '600px', // Adjust the width as needed
+                margin: '20px auto', // Centers the div
+                padding: '0 10px', // Optional padding
+            }}>
+                {/* Accordion for Instructions */}
+                <Accordion style={{ margin: '20px 0' }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center', // Center horizontally
+                            alignItems: 'center', // Center vertically
+                            textAlign: 'center', // Ensure text is centered
+                        }}
+                    >
+                        <Typography style={{ flex:1, color:'#54804D'}} >
+                            How to Use This Site
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Step 1: Customize your profile
+                            <br />
+                            Step 2: Add your favorite restaurants around you
+                            <br />
+                            Step 3: Add your availability
+                            <br />
+                            Step 4: Find matches
+                            <br />
+                            Step 5: Chat with your matches, go eat!
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+
+
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src={photo} alt="BowlMates banner" style={{ marginTop: '-150px' }} />
+            </div>
+
             <div style={{
                 display: 'flex',
                 flexWrap: 'wrap',
                 gap: '10px',
                 justifyContent: 'space-around'
             }}>
-            <Button onClick={() => navigateTo("find-restaurants")} variant="contained" style={{ backgroundColor: '#54804D', color: 'white' }}>
+                <Button onClick={() => navigateTo("find-restaurants")} variant="contained" style={{ backgroundColor: '#54804D', color: 'white' }}>
                     Find places to eat
                 </Button>
 
@@ -53,10 +103,6 @@ const Home = () => {
                 <Button onClick={() => navigateTo("settings")} variant="contained" style={{ backgroundColor: '#54804D', color: 'white' }}>
                     Edit your profile
                 </Button>
-            </div>
-
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img src={photo} alt="BowlMates banner" style={{ marginTop: '-100px' }} />
             </div>
 
         </>
